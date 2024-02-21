a8c350186ad60c1e7bdc68773f0ffc0a371fbf97
Kosmik56 <lewis.kidd56@gmail.com>
2024-02-21 14:07:14 +0100

    created phone entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,23 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Phone;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\Query\Expr\Math;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+            // Création d'une vingtaine de livres ayant pour titre
+            for ($i = 1; $i < 21; $i++) {
+                $phone = new Phone;
+                $phone->setName('Phone ' . $i);
+                $phone->setBrand('Brand');
+                $phone->setSerialNumber($i * rand(1,99999));
+                $manager->persist($phone);
+            }
+            $manager->flush();
+        }
     }
-}
---
+++ src/Entity/Phone.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhoneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhoneRepository::class)]
+class Phone
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand = null;
+
+    #[ORM\Column]
+    private ?int $serial_number = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): static
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getSerialNumber(): ?int
+    {
+        return $this->serial_number;
+    }
+
+    public function setSerialNumber(int $serial_number): static
+    {
+        $this->serial_number = $serial_number;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
